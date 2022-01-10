3954634095750ba0ee9878a1c1112b9c34e32c9a
DeamonSoul <81879340+DeamonSoul@users.noreply.github.com>
2022-01-10 17:49:33 +0400

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/AdminForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.INN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Income = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrePay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Credit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bonus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.INN,
+            this.Name,
+            this.Income,
+            this.PrePay,
+            this.Credit,
+            this.Bonus});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(1150, 350);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // INN
+            // 
+            this.INN.HeaderText = "ИНН";
+            this.INN.MinimumWidth = 6;
+            this.INN.Name = "INN";
+            this.INN.ReadOnly = true;
+            this.INN.Width = 125;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "ФИО";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            this.Name.Width = 125;
+            // 
+            // Income
+            // 
+            this.Income.HeaderText = "Доход";
+            this.Income.MinimumWidth = 6;
+            this.Income.Name = "Income";
+            this.Income.ReadOnly = true;
+            this.Income.Width = 125;
+            // 
+            // PrePay
+            // 
+            this.PrePay.HeaderText = "Ожидаемый налог";
+            this.PrePay.MinimumWidth = 6;
+            this.PrePay.Name = "PrePay";
+            this.PrePay.ReadOnly = true;
+            this.PrePay.Width = 125;
+            // 
+            // Credit
+            // 
+            this.Credit.HeaderText = "Задолженность";
+            this.Credit.MinimumWidth = 6;
+            this.Credit.Name = "Credit";
+            this.Credit.ReadOnly = true;
+            this.Credit.Width = 125;
+            // 
+            // Bonus
+            // 
+            this.Bonus.HeaderText = "Бонус на уплату налога";
+            this.Bonus.MinimumWidth = 6;
+            this.Bonus.Name = "Bonus";
+            this.Bonus.ReadOnly = true;
+            this.Bonus.Width = 125;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.dataGridView1);
+            this.MinimumSize = new System.Drawing.Size(1200, 900);
+            this.Text = "Система налогового учета \"Плати налог\"";
+            this.Resize += new System.EventHandler(this.AdminForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn INN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Income;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrePay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Credit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bonus;
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/DataBaseRequest.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    internal static class DataBaseRequest
+    {
+        /// <summary>
+        /// Выполняет команду и возвращает данные в виде DataTable
+        /// </summary>
+        /// <param name="commandStr">
+        /// Командная строка, в которой аргументы обозначаются как @P0, @P1 и т.д.
+        /// </param>
+        /// <param name="args">
+        /// Последовательность строковых значений соответствующих аргументов
+        /// </param>
+        /// <returns></returns>
+        public static DataTable AskForData(string commandStr, params string[] args)
+        {
+            DataBaseInit DB = new DataBaseInit();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand(commandStr, DB.GetConnector());
+            for (int i = 0; i< args.Length; i++)
+                command.Parameters.Add("@P" + i.ToString(), MySqlDbType.VarChar).Value = args[i];
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table;
+        }
+
+        public static void WriteNewUser(string inn, string name, string email, string password)
+        {
+            DataBaseInit DB = new DataBaseInit();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `usersdata` (`Inn`, `Name`, `Email`, `Password`, `Bonus`, `Income`, `Credit`, `Ask`, `Answer`) VALUES (@P0, @P1, @P2, @P3, '10000', '0', '0', '', '')", DB.GetConnector());
+
+            command.Parameters.Add("@P0", MySqlDbType.VarChar).Value = inn;
+            command.Parameters.Add("@P1", MySqlDbType.VarChar).Value = name;
+            command.Parameters.Add("@P2", MySqlDbType.VarChar).Value = email;
+            command.Parameters.Add("@P3", MySqlDbType.VarChar).Value = password;
+
+            DB.OpenConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Аккаунт был создан");
+            else
+                MessageBox.Show("Аккаунт не был создан");
+
+            //Создать таблицу для чеков
+            //CREATE TABLE `users`.`list111111111111` ( `Id` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT , `CustomerInn` VARCHAR(12) NOT NULL , `CustomerType` VARCHAR(1) NOT NULL , `ProductName` VARCHAR(50) NOT NULL , `Price` VARCHAR(15) NOT NULL, PRIMARY KEY (`ID`) ) ENGINE = InnoDB;
+
+            command = new MySqlCommand("CREATE TABLE `users`.`list" + inn.ToString() + "` ( `Id` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT , `CustomerInn` VARCHAR(12) NOT NULL , `CustomerType` VARCHAR(1) NOT NULL , `ProductName` VARCHAR(50) NOT NULL , `Price` VARCHAR(15) NOT NULL, PRIMARY KEY (`ID`) ) ENGINE = InnoDB", DB.GetConnector());
+
+            command.ExecuteNonQuery();
+
+            DB.CloseConnection();
+
+
+        }
+    }
+}
